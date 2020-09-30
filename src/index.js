cd752fb851221455e67cb6a8002bc3457d27bcd1
Petr <musakius.work@gmail.com>
2020-09-30 21:06:25 +0300

    feat: add tasktypical-arrays-problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array)) return 0;
+    if (array.length === 0) return 0;
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array)) return 0;
+    if (array.length === 0) return 0;
+    return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!Array.isArray(array)) return 0;
+    if (array.length === 0) return 0;
+    let result = array.reduce(function (a, b) {
+        return a + b;
+    });
+    return result / array.length;
+};
